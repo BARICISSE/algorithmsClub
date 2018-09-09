77740885ab7bd857e508471315d6d2d71601966b
Furaha <furahadamien30@gmail.com>
2018-09-08 20:57:19 -0400

    Kth largest element

--- algorithmsClub/src/algorithmsClub/Solutions.java
+++ algorithmsClub/src/algorithmsClub/Solutions.java
@@ -419,7 +419,7 @@ public class Solutions {
         return dissapearedNumbers;
     }
 	
-	
+	//find duplicates in an array
 	public List<Integer> findDuplicates(int[] nums) {
 		
 	    List<Integer> duplicates = new ArrayList<>();
@@ -434,7 +434,86 @@ public class Solutions {
 	    return duplicates;
 	}
 	
+	/*Kth Largest Element in an Array
+	 * Find the kth largest element in an unsorted array. 
+	 * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+	 */
+	public int findKthLargest(int[] nums, int k) {
+       int largestKth = 0;
+       
+       int [] sortedArr = quickSort(nums, 0, nums.length-1);
+       largestKth = sortedArr[nums.length - k];
+       
+       
+       return largestKth;
+		
+    }
 	
 	
+	//quick sort to help sort the elements
+	public static int[] quickSort(int arr[], int low, int high)
+	{
+		if (low < high)
+		{
+			/* pi is partitioning index, arr[pi] is 
+           now at right place */
+			int pi = partition(arr, low, high);
+
+			// Recursively sort elements before
+			// partition and after partition
+			quickSort(arr, low, pi-1);
+			quickSort(arr, pi+1, high);
+		}
+
+		return arr;
+	}
+	public static int partition(int arr[], int low, int high)
+	{
+		int pivot = arr[high]; 
+		int i = (low-1); // index of smaller element
+		for (int j=low; j<high; j++)
+		{
+			// If current element is smaller than or
+			// equal to pivot
+			if (arr[j] <= pivot)
+			{
+				i++;
+
+				// swap arr[i] and arr[j]
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+
+		// swap arr[i+1] and arr[high] (or pivot)
+		int temp = arr[i+1];
+		arr[i+1] = arr[high];
+		arr[high] = temp;
+
+		return i+1;
+	}
+	
+	public List<Interval> merge2(List<Interval> intervals) {
+        List<Interval> merged = new LinkedList<Interval>();
+        Collections.sort(intervals, new Comparator<Interval>(){
+           @Override
+            public int compare(Interval firstInterval, Interval secondInterval){
+                return firstInterval.start - secondInterval.start;
+            }
+        });
+        
+        for(Interval item: intervals){
+            if(item != null && merged.size() > 0 && merged.get(merged.size()-1).end < item.start){
+                merged.add(item); 
+            }else if(merged.size() > 0){
+                 merged.get(merged.size()-1).end = Math.max(item.end, merged.get(merged.size()-1).end);
+            }else{
+                merged.add(item);
+            }
+            
+        }
+        return merged;
+    }
 	
 }
